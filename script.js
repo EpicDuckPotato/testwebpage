6fa5e3e1e102b6e5a79200f6285600d9df7e0467
EpicDuckPotato <noopy.g.bhat@gmail.com>
2018-09-21 19:56:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 document.getElementById('date').innerHTML = new Date().toDateString();
-document.getElementById('nyancat').innerHTML = "nyan-cat-01-625x450.jpg";
+document.getElementById('nyancat').src = "nyan-cat-01-625x450.jpg";
